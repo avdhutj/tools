5329cb187f60b122bdb80e974a013591ac65c5cc
Avdhut Joshi <avdhut@Avdhuts-MacBook-Pro.local>
2014-11-19 23:25:05 -0500

    Initial Commit

---
+++ Tools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tools
+//
+//  Created by Avdhut Joshi on 11/19/14.
+//  Copyright (c) 2014 Fun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tools
+//
+//  Created by Avdhut Joshi on 11/19/14.
+//  Copyright (c) 2014 Fun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
