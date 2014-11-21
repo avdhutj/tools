9233cd4256582bd4c4a3503d1ebd5c1195e0468e
Mazin Biviji <mazinbiviji@Mazins-MacBook-Air.local>
2014-11-21 09:48:30 -0500

    updated storyboard

---
+++ Tools/InvList.h
@@ -0,0 +1,15 @@
+//
+//  InvList.h
+//  Tools
+//
+//  Created by Mazin Biviji on 11/21/14.
+//  Copyright (c) 2014 Fun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+
+@interface InvList : PFQuery
+
+
+@end
