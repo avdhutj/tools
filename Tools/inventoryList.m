9bd3dcbdf67b872f444546cf1b279d8cd4fb9eae
Mazin Biviji <mazinbiviji@Mazins-MacBook-Air.local>
2014-11-21 15:55:51 -0500

    Parse UI Library added and Inventory tab updated with pull to refresh

---
+++ Tools/inventoryList.m
@@ -0,0 +1,59 @@
+//
+//  inventoryList.m
+//  Tools
+//
+//  Created by Mazin Biviji on 11/21/14.
+//  Copyright (c) 2014 Fun. All rights reserved.
+//
+
+#import "inventoryList.h"
+
+@implementation inventoryList
+
+- (id)initWithCoder:(NSCoder *)aCoder {
+    self = [super initWithCoder:aCoder];
+    if (self) {
+        
+        self.parseClassName = @"invToolList";
+        self.pullToRefreshEnabled = YES;
+        self.paginationEnabled = YES;
+        self.objectsPerPage = 20;
+        
+    }
+    return self;
+}
+
+- (PFQuery *)queryForTable {
+    PFQuery *query = [PFQuery queryWithClassName:self.parseClassName];
+    
+    // If no objects are loaded in memory, we look to the cache
+    // first to fill the table and then subsequently do a query
+    // against the network.
+    if ([self.objects count] == 0) {
+        query.cachePolicy = kPFCachePolicyCacheThenNetwork;
+    }
+    
+    [query orderByDescending:@"createdAt"];
+    
+    return query;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath
+                        object:(PFObject *)object {
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle
+                                      reuseIdentifier:CellIdentifier];
+    }
+        
+        cell.textLabel.text = [object objectForKey:@"toolID"];
+        /*NSString * a = [object objectForKey:@"toolID"];
+        NSLog(@"%@",a);*/
+    
+    return cell;
+}
+
+@end
