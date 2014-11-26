77c88dac382f7154a815f6161b39b8400f5105ab
Mazin Biviji <mazinbiviji@Mazins-MacBook-Air.local>
2014-11-26 12:46:23 -0800

    Add Tool View Created and working w/o part number look up

--- Tools/AddToolViewController.h
+++ Tools/AddToolViewController.h
@@ -7,7 +7,24 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
 
 @interface AddToolViewController : UIViewController
+
+//Values set in the prepare to Segue function
+//@property* SupplierName;
+@property (strong, nonatomic) PFObject *Supplier;
+@property NSString* QRCode;
+
+
+//Values to be filled in from the Add tool View Controller
+@property (weak, nonatomic) IBOutlet UILabel *SupplierNameLbl;
+@property (weak, nonatomic) IBOutlet UILabel *SupplierAddressLbl;
+@property (weak, nonatomic) IBOutlet UITextField *ToolIdTxt;
+@property (weak, nonatomic) IBOutlet UITextField *WeightTxt;
+@property (weak, nonatomic) IBOutlet UITextField *DescrTxt;
+@property (weak, nonatomic) IBOutlet UITextField *PartNo1Txt;
+@property (weak, nonatomic) IBOutlet UITextField *PartNo2Txt;
+@property (weak, nonatomic) IBOutlet UIButton *AddToolBtn;
 
 @end
--- Tools/AddToolViewController.m
+++ Tools/AddToolViewController.m
@@ -17,12 +17,34 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    //These values should be set in the prepare for segue function from the Scan tab
+    PFQuery *query = [PFQuery queryWithClassName:@"SupplierList"];
+    self.Supplier = [query getObjectWithId:@"Pj9iWujEKk"];
+    
+    self.QRCode = @"QRCode_123";
+    //Set up for supplier name and address
+    self.SupplierNameLbl.text = [self.Supplier objectForKey:@"supplier"];
+    self.SupplierAddressLbl.text = [self.Supplier objectForKey:@"address"];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)AddToolBtnClicked:(id)sender {
+    NSLog(@"%@",self.Supplier.objectId);
+    //Need to perform basic validation to ensure all cells are filled
+    PFObject *tool = [PFObject objectWithClassName:@"Tools"];
+    tool[@"supplier"] = self.Supplier.objectId;
+    tool[@"toolId"] = self.ToolIdTxt.text;
+    tool[@"weight"] = [NSNumber numberWithInt:[self.WeightTxt.text integerValue]];
+    tool[@"toolDescription"] = self.DescrTxt.text;
+    [tool saveInBackground];
+    //Need to set up the part number adding thing this might be a little tricky because we will need to tool up the part number that was added and if it isnt there a new part number needs to be created after confirming with the user.
+}
+
 
 /*
 #pragma mark - Navigation
