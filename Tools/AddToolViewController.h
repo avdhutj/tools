0cbc407a5bff0416ee7c25fa4fd6e83d4f92ab11
Mazin Biviji <mazinbiviji@Mazins-MacBook-Air.local>
2014-11-25 22:39:01 -0800

    Added New tool View Controller

---
+++ Tools/AddToolViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddToolViewController.h
+//  Tools
+//
+//  Created by Mazin Biviji on 11/25/14.
+//  Copyright (c) 2014 Fun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddToolViewController : UIViewController
+
+@end
---
+++ Tools/AddToolViewController.m
@@ -0,0 +1,37 @@
+//
+//  AddToolViewController.m
+//  Tools
+//
+//  Created by Mazin Biviji on 11/25/14.
+//  Copyright (c) 2014 Fun. All rights reserved.
+//
+
+#import "AddToolViewController.h"
+
+@interface AddToolViewController ()
+
+@end
+
+@implementation AddToolViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
