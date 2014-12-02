ec28fc1b0f958d46081fdded6552f44467cf6cc2
Avdhut Joshi <avdhut@Avdhuts-MacBook-Pro.local>
2014-12-01 23:50:32 -0500

    Saving work..

--- InventoryListViewController.m
+++ InventoryListViewController.m
@@ -7,10 +7,13 @@
 //
 
 #import "InventoryListViewController.h"
-#import "AddToolTableViewController.h"
-#import "CameraViewController.h"
+
 #import <Parse/Parse.h>
 #import "LoadView.h"
+
+#import "AddToolTableViewController.h"
+#import "CameraViewController.h"
+#import "SupplierListViewController.h"
 
 @interface InventoryListViewController () {
 
@@ -21,9 +24,13 @@
 @property (strong, nonatomic) NSMutableArray* transferToolsArray;
 @property (strong, nonatomic) NSMutableArray* addToolsArray;
 
+@property (strong, nonatomic) PFGeoPoint* userLocation;
 @property (strong, nonatomic) NSMutableSet* doneSet;
 
+@property (nonatomic) ILControllerState controllerState;
+
 -(void)loadData;
+-(void)gotSupplier;
 
 @end
 
@@ -38,13 +45,27 @@
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
     
+    _userLocation = nil;
+    [PFGeoPoint geoPointForCurrentLocationInBackground:^(PFGeoPoint *geoPoint, NSError *error) {
+        _userLocation = [PFGeoPoint geoPointWithLatitude:[geoPoint latitude] longitude:[geoPoint longitude]];
+    }];
+    
     _allToolsArray = [[NSMutableArray alloc] init];
     _transferToolsArray = [[NSMutableArray alloc] init];
     _addToolsArray = [[NSMutableArray alloc] init];
     
     _doneSet = [[NSMutableSet alloc] init];
     
+    _controllerState = IL_NONE;
+    
     [self loadData];
+}
+
+-(void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    if (_controllerState == IL_SHIP_TOOL) {
+        [self gotSupplier];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
@@ -204,6 +225,45 @@
     [self.navigationController pushViewController:aTVC animated:YES];
 }
 
+-(void)HandleShipTool:(NSString*)qrCodeString{
+    PFQuery* query = [PFQuery queryWithClassName:@"Tools"];
+    [query whereKey:@"qrCode" equalTo:qrCodeString];
+    [query whereKey:@"toolId" equalTo:[_selectedObject objectForKey:@"toolId"]];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if (objects == 0) {
+            // Handle Error
+        }
+        else {
+            // Open Supplier List View Controller and wait for call from supplier selected method
+            _controllerState = IL_SHIP_TOOL;
+            _shippingSupplier = nil;
+            
+            SupplierListViewController* sLVC = [self.storyboard instantiateViewControllerWithIdentifier:@"SupplierListViewController"];
+            [sLVC setInventoryListViewController:self];
+            [self.navigationController pushViewController:sLVC animated:YES];
+        }
+    }];
+}
+
+-(void)HandleRecieveTool:(NSString*)qrCodeString {
+    PFQuery* query = [PFQuery queryWithClassName:@"Tools"];
+    [query whereKey:@"qrCode" equalTo:qrCodeString];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if (objects == 0) {
+            // Handle Error
+        }
+        else {
+            // Show alert confirming recieved tool from supplier
+            // Set current geo location and save the object
+            [[[UIAlertView alloc] initWithTitle:@"Reciept" message:@"Recieved tool from Supplier" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil] show];
+            PFObject* toolObject = [objects objectAtIndex:0];
+            [toolObject setObject:_userLocation forKey:@"toolGeoPoint"];
+            [toolObject saveEventually];
+        }
+    }];
+    
+}
+
 -(void)gotQRCode:(NSString *)qrCodeString {
     if (qrCodeString == nil) {
         // Process Error
@@ -216,7 +276,12 @@
         }
         else if ([[_selectedObject valueForKey:@"taskType"] isEqualToNumber:[NSNumber numberWithInt:1]]) {
             [self HandleAddTool:qrCodeString];
-            
+        }
+        else if ([[_selectedObject valueForKey:@"taskType"] isEqualToNumber:[NSNumber numberWithInt:2]]) {
+            [self HandleShipTool:qrCodeString];
+        }
+        else if ([[_selectedObject valueForKey:@"taskType"] isEqualToNumber:[NSNumber numberWithInt:3]]) {
+            [self HandleRecieveTool:qrCodeString];
         }
         
     }
@@ -224,6 +289,17 @@
 
 -(void)SaveSucess {
     [_doneSet addObject:_selectedObject];
+}
+
+-(void)gotSupplier {
+    _controllerState = IL_NONE;
+    if (_shippingSupplier) {
+        // Format string
+        UIAlertView* alert = [[UIAlertView alloc] initWithTitle:@"Shipping" message:@"Shipping to supplier" delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Ok", nil];
+        [alert setTag:1];
+        [alert show];
+    }
+    
 }
 
 
